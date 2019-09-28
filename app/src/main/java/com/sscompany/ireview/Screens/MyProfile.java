77afb6a28e5575ffa7564c4482e6987f9d2eedb7
Ali Babayev <ali.babayev@ug.bilkent.edu.tr>
2019-09-28 14:48:58 +0300

    reset myprofile

--- app/src/main/java/com/sscompany/ireview/Screens/MyProfile.java
+++ app/src/main/java/com/sscompany/ireview/Screens/MyProfile.java
@@ -80,10 +80,6 @@ public class MyProfile extends AppCompatActivity {
                         {
                             Book book = sampleDataSnapshot.getValue(Book.class);
                             bookList.add(book);
-                            if(dataSnapshot.getChildrenCount() == 0)
-                            {
-                                findViewById(R.id.no_book).setVisibility(View.VISIBLE);
-                            }
                         }
 
                         if(dataSnapshot.getChildrenCount() == 0)
